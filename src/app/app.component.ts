6e7d2e19703f1fc4c06d4307be382536ef6cc884
luisa <luojeda98@hotmail.com>
2023-02-28 11:04:27 -0500

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Capitán América';
+  nombre2: string = 'ferNanDo hErrerA jImeneZ';
+  personajes: string[] = ['Ironman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
+  PI: number = Math.PI;
+  porcentaje: number = 0.235;
+  salario: number = 1234.5
 }
